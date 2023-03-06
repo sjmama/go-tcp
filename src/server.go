51b90674aa86f827c15e9b7be669503dc839f34d
sjmama <ykh45sko@gmail.com>
2023-03-06 18:47:19 +0900

    done

--- src/server.go
+++ src/server.go
@@ -6,7 +6,6 @@ import (
 )
 
 func main() {
-	flag := true
 	link, err := net.Listen("tcp", "127.0.0.8:8080") //link 랑 error
 	if err != nil {
 		fmt.Println(err)
@@ -18,30 +17,24 @@ func main() {
 
 	for {
 		conn, err := link.Accept() //이제 이 con으로 주고 받고 하면됨
-		if flag == false {
-			fmt.Println("00000000000")
-			break
-		}
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
 		defer link.Close()
 
-		go requesthandler(conn, &flag)
+		go requesthandler(link, conn)
 		fmt.Println("8888888888888")
 	}
 }
 
-func requesthandler(conn net.Conn, flag *bool) {
+func requesthandler(link net.Listener, conn net.Conn) {
 	data := make([]byte, 4096)
 	for {
 		n, err := conn.Read(data) //읽은 바이트 수랑 에러
 		if err != nil {
-
-			*flag = false
+			link.Close()
 			fmt.Println(err)
-			fmt.Println(*flag)
 			return
 		}
 
@@ -50,9 +43,7 @@ func requesthandler(conn net.Conn, flag *bool) {
 
 		_, err = conn.Write(data)
 		if err != nil {
-			*flag = false
 			fmt.Println(err)
-			fmt.Println(*flag)
 			return
 		}
 	}
